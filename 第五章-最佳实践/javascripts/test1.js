1af8260270871b142e96e9a7e63e51b934b091ea
zhentian sun <BlackGoblin>
2018-03-02 18:16:34 +0800

    第五章 完结

--- 第五章-最佳实践/javascripts/test1.js
+++ 第五章-最佳实践/javascripts/test1.js
@@ -1,27 +1,31 @@
-function popUP(winURL){
+function popUp(winURL){
     window.open(winURL, "popup", "width=320,height=480");
 }
 
-// window.onload = prepareLinks;
-// function prepareLinks(){
-//     var links = document.getElementsByClassName("popup");
-//     for (var i = 0; i < links.length; i++){
-//         links[i].onclick = function(){
-//             popUp(this.getAttribute("href"));
-//             return false;
-//         }
-//     }
-// }
-
 window.onload = prepareLinks;
 function prepareLinks(){
-    var links = document.getElementsByTagName("a");
+    if (!document.getElementsByClassName) return false;
+    var links = document.getElementsByClassName("popup");
     for (var i = 0; i < links.length; i++){
         if (links[i].getAttribute("class") == "popup"){
             links[i].onclick = function(){
-                popUP(this.getAttribute("href"));
+                popUp(this.getAttribute("href"));
                 return false;
             }
         }
     }
 }
+
+// window.onload = prepareLinks;
+// function prepareLinks(){
+//     if (!document.getElementsByTagName) return false;
+//     var links = document.getElementsByTagName("a");
+//     for (var i = 0; i < links.length; i++){
+//         if (links[i].getAttribute("class") == "popup"){
+//             links[i].onclick = function(){
+//                 popUp(this.getAttribute("href"));
+//                 return false;
+//             }
+//         }
+//     }
+// }
