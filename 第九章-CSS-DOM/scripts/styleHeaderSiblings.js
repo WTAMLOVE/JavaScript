6000be8aa12ca96ca00979c7a2b52e8ab5166aa6
zhentian sun <BlackGoblin>
2018-03-13 20:00:31 +0800

    第九章 开始

---
+++ 第九章-CSS-DOM/scripts/styleHeaderSiblings.js
@@ -0,0 +1,35 @@
+addLoadEvent(styleHeaderSiblings);
+function styleHeaderSiblings(){
+    if (!document.getElementsByTagName) return false;
+
+    var headers = document.getElementsByTagName("h1");
+    var elem;
+    for (var i = 0; i < headers.length; i++){
+        elem = getNextElement(headers[i].nextSibling);
+
+        elem.style.fontWeight = "bold";
+        elem.style.fontSize = "1.2em";
+    }
+}
+
+function getNextElement(node){
+    if (node.nodeType == 1){
+        return node;
+    }
+    if (node.nextSibling){
+        return getNextElement(node.nextSibling);
+    }
+    return null;
+}
+
+function addLoadEvent(func){
+    var oldOnload = window.onload;
+    if (typeof oldOnload != "function"){
+        window.onload = func;
+    }else{
+        window.onload = function(){
+            oldOnload();
+            func();
+        }
+    }
+}
